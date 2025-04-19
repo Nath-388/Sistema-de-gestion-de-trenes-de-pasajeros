bd6a338cdd6b2eabf5e197267b9fdd9a964b1cf6
Nath-388 <nataliasofiarojaspimienta@gmail.com>
2025-04-19 08:35:55 -0500

    Implementación del generador de ids

--- src/ModuloUtillidades/GeneradorIDs.java
+++ src/ModuloUtillidades/GeneradorIDs.java
@@ -1,5 +1,21 @@
 package ModuloUtillidades;
 
+import java.util.Random;
+
 public class GeneradorIDs {
-    //es para generar automaticamente los ids de los objetos
+    private static final String LOWERCASE = "abcdefghijkmnlopqrstuvwxyz";
+    private static final String UPPERCASE= "ABCDEFGHIJKMNLOPQRSTUVWXYZ";
+    private static final String DIGITS = "0123456789";
+
+    public static String generateID(int length) {
+        String characters= LOWERCASE + UPPERCASE + DIGITS;
+        char[] id = new char[length];
+        int pos;
+        Random r= new Random();
+        for (int i = 0; i < length; i++) {
+            pos= r.nextInt(characters.length());
+            id[i] = characters.charAt((pos));
+        }
+        return new String(id);
+    }
 }
